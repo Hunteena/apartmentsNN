713adbfe21814ab88e993c624222dcdcbee406e4
Nina Spernaskaya <nina-sp@list.ru>
2023-03-21 18:19:18 +0600

    Fix string to list representation in schema

---
+++ apartmentsNN/backend/schema.py
@@ -0,0 +1,37 @@
+BOOKING_SCHEMA_EXAMPLE = {
+    "id": 0,
+    "images": [
+        {
+            "photo": "string",
+            "name": "string",
+            "altName": "string"
+        }
+    ],
+    "detailedCharacteristic": [
+        {
+            "name": "string",
+            "data": "string"
+        }
+    ],
+    "comfort": [
+        {
+            "type": "string",
+            "description": "string"
+        }
+    ],
+    "location": {
+        "url": "string",
+        "desc": [
+            "string"
+        ]
+    },
+    "name": "string",
+    "title": "string",
+    "address": "string",
+    "description": "string",
+    "shortDescription": [
+        "string"
+    ],
+    "price": 2147483647,
+    "capacity": 32767
+}
--- apartmentsNN/flats/views.py
+++ apartmentsNN/flats/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from drf_spectacular.utils import extend_schema, inline_serializer
+from drf_spectacular.utils import extend_schema, inline_serializer, \
+    OpenApiExample
 from rest_framework import fields
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import Apartment
-from .serializers import ApartmentSerializer
+from flats.models import Apartment
+from flats.serializers import ApartmentSerializer
+from backend.schema import BOOKING_SCHEMA_EXAMPLE
 
 
+@extend_schema(
+    examples=[OpenApiExample('response', value=BOOKING_SCHEMA_EXAMPLE)]
+)
 class ApartmentViewSet(ReadOnlyModelViewSet):
     """
     Класс для просмотра информации о квартирах
